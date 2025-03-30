69538771e52ae4fefab0b804db250a04ccce3a65
Vallary Awuor Onyando <valonyando@gmail.com>
2025-03-30 14:28:05 +0300

    Modifies object

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+let employee = {
+        name : "Val",
+        streetAddress : "Leeds",
+}
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let newEmployee = {...employee}
+    return newEmployee = {name : "Sam", streetAddress : "11 Broadway"};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee["name"] = "Sam";
+    employee["streetAddress"] = "12 Broadway";
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let copyEmployee = {...employee};
+    delete copyEmployee.name;
+    return copyEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee.name;
+    return employee;
+}
